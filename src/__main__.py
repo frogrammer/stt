acc85c9e9a01dcee65d9e95ca27b9680c588abfb
Luke Vinton <luvinton@microsoft.com>
2022-01-12 16:04:50 +0000

    prototype wip

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,19 @@
+""" main stt application.
+"""
+import os
+
 import azure.cognitiveservices.speech as speechsdk
 
-def from_file():
-    speech_config = speechsdk.SpeechConfig(subscription="<paste-your-speech-key-here>", region="<paste-your-speech-location/region-here>")
-    audio_input = speechsdk.AudioConfig(filename="your_file_name.wav")
+SPEECH_KEY = os.getenv('SPEECH_KEY')
+REGION = os.getenv('REGION')
+
+def from_file(in_path: str, out_path: str):
+    """ take audio/video from in_path and generate assets in out_path 
+    """
+
+    speech_config = speechsdk.SpeechConfig(subscription=SPEECH_KEY, region=REGION)
+    audio_input = speechsdk.AudioConfig(filename="in_path")
     speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
 
     result = speech_recognizer.recognize_once_async().get()
     print(result.text)
-
-from_file()
