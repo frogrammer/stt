24d42920c96c65dfbe5a8243aed5ceabdb8a5198
Luke Vinton <luvinton@microsoft.com>
2022-02-23 16:10:53 +0000

    loop process no longer needed

--- app/src/process.py
+++ app/src/process.py
@@ -5,13 +5,11 @@ from time import sleep
 import task
 
 def main():
-    while True:
-        for f in os.listdir(task.IN_DIR):
-            abs_path = Path(task.IN_DIR, f)
-            task.create_task(abs_path)
-        for task_id in task.get_tasks():
-            task.process_sync(task_id)
-        sleep(15)
+    for f in os.listdir(task.IN_DIR):
+        abs_path = Path(task.IN_DIR, f)
+        task.create_task(abs_path)
+    for task_id in task.get_tasks():
+        task.process_sync(task_id)
         
 
 if __name__ == "__main__":
